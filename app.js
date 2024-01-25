e560138f288307a0ed55818e15c9d1e9d258b513
Fewatts <feralveswatts@gmail.com>
2024-01-25 16:27:39 -0300

    Parte 3 do desafio
    integração das funções no console

--- app.js
+++ app.js
@@ -27,3 +27,37 @@ const descriptografar = (criptografada) => {
       (match) => substituicoesInvertidas[match]
    );
 };
+
+const validarEntrada = (entrada) => {
+   var caracteresEspeciais = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
+   var letrasMaiusculas = /[A-Z]/;
+   var apenasMinusculas = /^[a-z]+$/;
+
+   if (
+      caracteresEspeciais.test(entrada) ||
+      letrasMaiusculas.test(entrada) ||
+      !apenasMinusculas.test(entrada)
+   ) {
+      return false;
+   } else {
+      return true;
+   }
+};
+
+const processamentoCriptografar = () => {
+   let inputUsuario = document.getElementById("input").value;
+   if (validarEntrada(inputUsuario)) {
+      console.log(criptografar(inputUsuario));
+   } else {
+      console.log("hmmm tem coisa errada ai");
+   }
+};
+
+const processamentoDescriptografar = () => {
+   let inputUsuario = document.getElementById("input").value;
+   if (validarEntrada(inputUsuario)) {
+      console.log(descriptografar(inputUsuario));
+   } else {
+      console.log("hmmm tem coisa errada ai");
+   }
+};
